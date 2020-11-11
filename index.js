3bcf30099978aacd1f434c9365c40634bb77e2ed
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-11-11 11:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Write your code here!
 main.remove();
 
-document.body.appendChild(element);
-
 const newHeader = document.createElement('h1#victory');
-
-document.appendChild(newHeader);
+document.body.appendChild(element);
